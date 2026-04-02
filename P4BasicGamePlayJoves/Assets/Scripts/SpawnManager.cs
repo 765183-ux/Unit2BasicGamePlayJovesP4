da8d337b06e453820698cd1dffcf9c856de93a97
765183-ux <765183@student.dusd.net>
2026-04-01 19:14:46 -0700

    Finishing my project
    Adding last details

--- P4BasicGamePlayJoves/Assets/Scripts/SpawnManager.cs
+++ P4BasicGamePlayJoves/Assets/Scripts/SpawnManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-
+  
     // Start is called once before the first execution of Update after the MonoBehavior is created
     void Start()
     {
@@ -13,6 +13,11 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+           
+            Instantiate(animalPrefabs[animalIndex], new  Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
